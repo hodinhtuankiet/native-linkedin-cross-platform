b4dc8e3788efde780540b7a535145e958271c12f
DTK-18 <120298180+hodinhtuankiet@users.noreply.github.com>
2024-04-20 23:55:56 +0700

    UI Input Text & Emoji Icon

--- app/(tab)/home/ChatMessage.js
+++ app/(tab)/home/ChatMessage.js
@@ -23,8 +23,16 @@ import { Entypo } from "@expo/vector-icons";
 // import EmojiSelector from "react-native-emoji-selector";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import * as ImagePicker from "expo-image-picker";
+import EmojiSelector from "react-native-emoji-selector";
 
 const ChatMessage = () => {
+  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
+  const [selectedMessages, setSelectedMessages] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const handleEmojiPress = () => {
+    setShowEmojiSelector(!showEmojiSelector);
+  };
   return (
     <KeyboardAvoidingView style={{ flex: 1, backgroundColor: "#F0F0F0" }}>
       <ScrollView></ScrollView>
@@ -36,33 +44,65 @@ const ChatMessage = () => {
           paddingVertical: 10,
           borderTopWidth: 1,
           borderTopColor: "#dddddd",
-          // marginBottom: showEmojiSelector ? 0 : 25,
+          marginBottom: showEmojiSelector ? 0 : 25,
         }}
       >
         <Entypo
-          // onPress={handleEmojiPress}
+          onPress={handleEmojiPress}
           style={{ marginRight: 5 }}
           name="emoji-happy"
           size={24}
           color="gray"
         />
         <TextInput
-          // value={message}
-          // onChangeText={(text) => setMessage(text)}
+          value={message}
+          onChangeText={(text) => setMessage(text)}
           style={{
             flex: 1,
             height: 40,
             borderWidth: 1,
             borderColor: "#dddddd",
             borderRadius: 20,
-            paddingHorizontal: 10,
+            paddingHorizontal: 7,
           }}
           placeholder="Type Your message..."
         />
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 7,
+            marginHorizontal: 8,
+          }}
+        >
+          <Entypo
+            // onPress={pickImage}
+            name="camera"
+            size={24}
+            color="gray"
+          />
+          <Feather name="mic" size={24} color="gray" />
+        </View>
+        <Pressable
+          onPress={() => handleSend("text")}
+          style={{
+            backgroundColor: "#007bff",
+            paddingVertical: 8,
+            paddingHorizontal: 12,
+            borderRadius: 20,
+          }}
+        >
+          <Text style={{ color: "white", fontWeight: "bold" }}>Send</Text>
+        </Pressable>
       </View>
-      {/* <Entypo name="camera" size={24} color="gray" />
-
-      <Feather name="mic" size={24} color="gray" /> */}
+      {showEmojiSelector && (
+        <EmojiSelector
+          onEmojiSelected={(emoji) => {
+            setMessage((prevMessage) => prevMessage + emoji);
+          }}
+          style={{ height: 250 }}
+        />
+      )}
     </KeyboardAvoidingView>
   );
 };
